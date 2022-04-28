dc41fbe8ed0d381df9a9e537ac649225e7a9f13e
Wellington Castro <wellingtonvcastro@gmail.com>
2022-04-28 14:12:26 -0300

    update setup script

--- wellington-basic-setup.sh
+++ wellington-basic-setup.sh
@@ -41,40 +41,30 @@ cd ..
 rm -rf fonts
 
 # Brave web browser
-sudo apt install apt-transport-https curl gnupg -y
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install brave-browser -y
+#sudo snap install brave
 
 # Bitwarden password manager
-snap install bitwarden
+sudo snap install bitwarden
 
 # tldr cli helper
 sudo apt install tldr -y
 
 # Spotify
-snap install spotify
-
-# lxc containers
-snap install lxd
+sudo snap install spotify
 
 # Flat Remix theme
-mkdir ~/.themes
-mkdir ~/.icons
-git clone https://github.com/daniruiz/flat-remix.git --depth 1
-git clone https://github.com/daniruiz/flat-remix-gtk.git --depth 1
-cp -r flat-remix/Flat-Remix* ~/.icons/
-cp -r flat-remix-gtk/Flat-Remix* ~/.themes/ 
-rm -rf flat-remix
-rm -rf flat-remix-gnome
-sudo apt install gnome-tweak-tool -y
+#mkdir ~/.themes
+#mkdir ~/.icons
+#git clone https://github.com/daniruiz/flat-remix.git --depth 1
+#git clone https://github.com/daniruiz/flat-remix-gtk.git --depth 1
+#cp -r flat-remix/Flat-Remix* ~/.icons/
+#cp -r flat-remix-gtk/Flat-Remix* ~/.themes/ 
+#rm -rf flat-remix
+#rm -rf flat-remix-gnome
+sudo apt install gnome-tweaks
 
 # Signal messenger
-wget -O- https://updates.signal.org/desktop/apt/keys.asc | \
-sudo apt-key add -
-echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" \ | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
-sudo apt update && sudo apt install signal-desktop -y
+sudo snap install signal-desktop
 
 # discord
 snap install discord
@@ -88,6 +78,12 @@ sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools
 wget https://raw.githubusercontent.com/wvmcastro/misc/main/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-sudo apt install python3-dev python3-pip python3-setuptools
-sudo pip3 install thefuck
+sudo apt install python3-dev python3-pip python3-setuptools -y
+sudo pip3 install thefuck -y
+sudo chsh -s $(which zsh)
 
+# tmux
+sudo apt install tmux
+
+# vscode
+#sudo snap install code --classic
